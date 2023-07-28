d6d8d48b68dbaa33fd9730447f4b7b25e5a59151
Park-MinJe <phone7101@naver.com>
2023-07-28 14:14:20 +0900

    update project structure

---
+++ src/main/java/com/WhatIsMethIs/WhatIsMethIsApplication.java
@@ -0,0 +1,13 @@
+package com.WhatIsMethIs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatIsMethIsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatIsMethIsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ossp2023/WhatIsMethIs/WhatIsMethIsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ossp2023.WhatIsMethIs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatIsMethIsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
