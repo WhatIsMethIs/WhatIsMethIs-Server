a9254d5b0b37117bed5dd9ed27ffeef32083f929
jiholee0 <dlwlgh1254@naver.com>
2023-08-05 23:28:45 +0900

    update user api(1.3.4 비밀번호 수정, 1.3.5 로그아웃)

--- src/main/java/com/WhatIsMethIs/src/controller/UserController.java
+++ src/main/java/com/WhatIsMethIs/src/controller/UserController.java
@@ -4,10 +4,10 @@ import com.WhatIsMethIs.config.BaseException;
 import com.WhatIsMethIs.config.BaseResponse;
 import com.WhatIsMethIs.src.dto.user.*;
 import com.WhatIsMethIs.src.entity.User;
-import com.WhatIsMethIs.src.service.SocialLoginService;
 import com.WhatIsMethIs.src.service.UserService;
 import com.WhatIsMethIs.utils.TokenUtils;
 import io.swagger.v3.oas.annotations.*;
+import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -25,14 +25,11 @@ public class UserController {
     @Autowired
     private final UserService userService;
     @Autowired
-    private final SocialLoginService socialLoginService;
-    @Autowired
     private final TokenUtils tokenUtils;
 
 
-    public UserController(UserService userService, SocialLoginService socialLoginService, TokenUtils tokenUtils) {
+    public UserController(UserService userService, TokenUtils tokenUtils) {
         this.userService = userService;
-        this.socialLoginService = socialLoginService;
         this.tokenUtils = tokenUtils;
     }
 
@@ -42,10 +39,10 @@ public class UserController {
      * [GET] /users
      */
     @Operation(method = "GET",
-            description = "유저 전체 조회 api", tags = "USER", summary = "1.1.1 유저 전체 조회")
+            description = "유저 전체를 조회하는 api로, USER 객체 그대로 반환합니다.", tags = "USER", summary = "1.1.1 유저 전체 조회")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "1000", description = "요청에 성공하였습니다."),
-            @ApiResponse(responseCode = "4000", description = "데이터베이스 연결에 실패하였습니다.")
+            @ApiResponse(responseCode = "4000", description = "데이터베이스 연결에 실패하였습니다.", content = @Content())
     })
     @ResponseBody
     @GetMapping("")
@@ -63,10 +60,12 @@ public class UserController {
      * [GET] /users/:index
      */
     @Operation(method = "GET",
-            description = "유저 1명 조회 api", tags = "USER", summary = "1.1.2 유저 index로 유저 1명 조회")
+            description = "유저 1명을 조회하는 api로, USER 객체 그대로 반환합니다. 단, index 값에 해당하는 유저가 없을 경우 에러 반환(code=3005) " +
+                    "* nullable : password, emerge~, refreshToken, deviceToken", tags = "USER", summary = "1.1.2 유저 index로 유저 1명 조회")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "1000", description = "요청에 성공하였습니다."),
-            @ApiResponse(responseCode = "4000", description = "데이터베이스 연결에 실패하였습니다.")
+            @ApiResponse(responseCode = "3005", description = "유저가 존재하지 않습니다.", content = @Content()),
+            @ApiResponse(responseCode = "4000", description = "데이터베이스 연결에 실패하였습니다.", content = @Content())
     })
     @ResponseBody
     @GetMapping("/{index}")
@@ -77,7 +76,6 @@ public class UserController {
         } catch (BaseException exception) {
             return new BaseResponse<>((exception.getStatus()));
         }
-
     }
 
 
@@ -86,16 +84,22 @@ public class UserController {
      * [POST] /users
      */
     @Operation(method = "POST",
-            description = "이름, 이메일, 비밀번호, 나이, 휴대폰번호로 회원가입 api", tags = "USER", summary = "1.2.1 회원가입")
+            description = "이름, 이메일, (비밀번호), 나이, 휴대폰번호로 회원가입하는 api이며, 소셜로그인인 경우 비밀번호는 공란으로 넣어주시면 됩니다. ",
+            tags = "USER", summary = "1.2.1 회원가입")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "1000", description = "요청에 성공하였습니다."),
-            @ApiResponse(responseCode = "4000", description = "데이터베이스 연결에 실패하였습니다.")
+            @ApiResponse(responseCode = "2011", description = "이메일 형식을 확인해주세요.", content = @Content()),
+            @ApiResponse(responseCode = "2012", description = "전화번호 형식을 확인해주세요.", content = @Content()),
+            @ApiResponse(responseCode = "2013", description = "이미 가입한 이메일입니다.", content = @Content()),
+            @ApiResponse(responseCode = "4000", description = "데이터베이스 연결에 실패하였습니다.", content = @Content()),
+            @ApiResponse(responseCode = "4010", description = "비밀번호 암호화에 실패하였습니다.", content = @Content())
+
     })
     @ResponseBody
     @PostMapping("")
-    public BaseResponse<PostRes> createUser(@Parameter @RequestBody PostReq postReq) {
+    public BaseResponse<PostRes> createUser(@Parameter @RequestBody PostJoinReq postJoinReq) {
         try {
-            PostRes postRes = userService.createUser(postReq);
+            PostRes postRes = userService.createUser(postJoinReq);
             return new BaseResponse<>(postRes);
         } catch (BaseException exception) {
             return new BaseResponse<>((exception.getStatus()));
@@ -108,29 +112,35 @@ public class UserController {
      * [POST] /users/login?code={loginCode}
      */
     @Operation(method = "POST",
-            description = "loginCode(email/kakao/apple)에 따라 로그인 api", tags = "USER", summary = "1.2.2 로그인")
+            description = "loginCode에 따라 로그인하는 api이며, 소셜로그인 선택한 유저 중 회원이 아닐 경우 id = -1로 return합니다. (즉, id = -1일 경우 회원가입 절차 밟아야 함)", tags = "USER", summary = "1.2.2 로그인")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "1000", description = "요청에 성공하였습니다."),
-            @ApiResponse(responseCode = "2010", description = "이메일을 입력해주세요."),
-            @ApiResponse(responseCode = "2011", description = "이메일 형식을 확인해주세요."),
-            @ApiResponse(responseCode = "4000", description = "데이터베이스 연결에 실패하였습니다."),
-            @ApiResponse(responseCode = "4001", description = "데이터가 존재하지 않습니다.")
+            @ApiResponse(responseCode = "2010", description = "이메일을 입력해주세요.", content = @Content()),
+            @ApiResponse(responseCode = "2011", description = "이메일 형식을 확인해주세요.", content = @Content()),
+            @ApiResponse(responseCode = "3000", description = "없는 이메일이거나 비밀번호가 틀렸습니다.", content = @Content()),
+            @ApiResponse(responseCode = "3001", description = "이메일 정보를 가져오는데 실패하였습니다.", content = @Content()),
+            @ApiResponse(responseCode = "3002", description = "카카오 api 호출 응답 정보를 불러오는데 실패하였습니다.", content = @Content()),
+            @ApiResponse(responseCode = "3003", description = "애플 api 호출 응답 정보를 불러오는데 실패하였습니다.", content = @Content()),
+            @ApiResponse(responseCode = "3004", description = "애플 api 호출 중 사용 가능한 공개키가 없어 실패하였습니다.", content = @Content()),
+            @ApiResponse(responseCode = "3006", description = "이메일 가입 사용자입니다.", content = @Content()),
+            @ApiResponse(responseCode = "4000", description = "데이터베이스 연결에 실패하였습니다.", content = @Content()),
+            @ApiResponse(responseCode = "4011", description = "비밀번호 복호화에 실패하였습니다.", content = @Content())
     })
     @ResponseBody
     @PostMapping("/login")
-    public BaseResponse<PostRes> login(@Parameter(required = true, name="loginCode") @RequestParam String loginCode, @Parameter @RequestBody(required = false) PostLoginReq postLoginReq) {
+    public BaseResponse<PostRes> login(@Parameter(required = true, name="loginCode", example = "email/kakao/apple") @RequestParam String loginCode, @Parameter @RequestBody(required = false) PostLoginReq postLoginReq) {
         try {
             PostRes postRes;
             if (loginCode == null) {
                 throw new BaseException(INVALID_REQ_PARAM);
-            } else if (loginCode.equals("email")) {
+            } else if (loginCode.equalsIgnoreCase("email")) {
                 // 기본 로그인
                 postRes = userService.emailLogin(postLoginReq);
-            } else if(loginCode.equals("kakao")) {
+            } else if(loginCode.equalsIgnoreCase("kakao")) {
                 // 카카오 로그인
                 String accessToken = tokenUtils.getAccessToken();
                 postRes = userService.kakaoLogin(accessToken);
-            } else if(loginCode.equals("apple")) {
+            } else if(loginCode.equalsIgnoreCase("apple")) {
                 // 애플 로그인
                 String accessToken = tokenUtils.getAccessToken();
                 postRes = userService.appleLogin(accessToken);
@@ -151,14 +161,14 @@ public class UserController {
      * [POST] /users/refreshToken
      */
     @Operation(method = "POST",
-            description = "Header-'X-ACCESS-TOKEN'에 refreshToken 값을 넣어 자동 로그인 api(jwt와 refresh token 모두 새로 발급)",
-            tags = "USER", summary = "Refresh Token 로그인 API - \uD83D\uDD12 refreshToken")
+            description = "Header-'X-ACCESS-TOKEN'에 refreshToken 값을 넣어 자동 로그인하는 api이며, jwt와 refresh token 모두 새로 발급합니다.",
+            tags = "USER", summary = "1.2.3 Refresh Token 로그인 - \uD83D\uDD12 refreshToken")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "1000", description = "요청에 성공하였습니다."),
-            @ApiResponse(responseCode = "2000", description = "JWT를 입력해주세요."),
-            @ApiResponse(responseCode = "2001", description = "유효하지 않은 JWT입니다."),
-            @ApiResponse(responseCode = "4000", description = "데이터베이스 연결에 실패하였습니다."),
-            @ApiResponse(responseCode = "4001", description = "데이터가 존재하지 않습니다.")
+            @ApiResponse(responseCode = "2000", description = "JWT를 입력해주세요.", content = @Content()),
+            @ApiResponse(responseCode = "2001", description = "유효하지 않은 JWT입니다.", content = @Content()),
+            @ApiResponse(responseCode = "3005", description = "유저가 존재하지 않습니다.", content = @Content()),
+            @ApiResponse(responseCode = "4000", description = "데이터베이스 연결에 실패하였습니다.", content = @Content())
     })
     @ResponseBody
     @PostMapping("/refreshToken")
@@ -179,20 +189,24 @@ public class UserController {
      * [PATCH] /users
      */
     @Operation(method = "PATCH",
-            description = "Header-'X-ACCESS-TOKEN'에 JWT 값을 넣어 유저 정보(이메일, 이름, 나이, 전화번호) 수정 api",
+            description = "Header-'X-ACCESS-TOKEN'에 JWT 값을 넣어 유저 정보(이메일, 이름, 나이, 전화번호)를 수정하는 api입니다.",
             tags = "USER", summary = "1.3.1 유저 정보 수정 - \uD83D\uDD12 JWT")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "1000", description = "요청에 성공하였습니다."),
-            @ApiResponse(responseCode = "2000", description = "JWT를 입력해주세요."),
-            @ApiResponse(responseCode = "2001", description = "유효하지 않은 JWT입니다."),
-            @ApiResponse(responseCode = "4000", description = "데이터베이스 연결에 실패하였습니다.")
+            @ApiResponse(responseCode = "2000", description = "JWT를 입력해주세요.", content = @Content()),
+            @ApiResponse(responseCode = "2001", description = "유효하지 않은 JWT입니다.", content = @Content()),
+            @ApiResponse(responseCode = "2011", description = "이메일 형식을 확인해주세요.", content = @Content()),
+            @ApiResponse(responseCode = "2012", description = "전화번호 형식을 확인해주세요.", content = @Content()),
+            @ApiResponse(responseCode = "2013", description = "이미 가입한 이메일입니다.", content = @Content()),
+            @ApiResponse(responseCode = "3005", description = "유저가 존재하지 않습니다.", content = @Content()),
+            @ApiResponse(responseCode = "4000", description = "데이터베이스 연결에 실패하였습니다.", content = @Content())
     })
     @ResponseBody
     @PatchMapping("")
-    public BaseResponse<PatchRes> modifyUser(@Parameter(required = true) @RequestBody PatchReq patchReq) {
+    public BaseResponse<PatchRes> modifyUser(@Parameter(required = true) @RequestBody PatchUserInfoReq patchUserInfoReq) {
         try {
             int userIdByJwt = tokenUtils.getUserId();
-            PatchRes patchRes = userService.modifyUser(userIdByJwt, patchReq);
+            PatchRes patchRes = userService.modifyUser(userIdByJwt, patchUserInfoReq);
             return new BaseResponse<>(patchRes);
 
         } catch (BaseException exception) {
@@ -206,13 +220,15 @@ public class UserController {
      * [PATCH] /users
      */
     @Operation(method = "PATCH",
-            description = "Header-'X-ACCESS-TOKEN'에 JWT 값을 넣어 비상연락망 등록 및 수정 api",
+            description = "Header-'X-ACCESS-TOKEN'에 JWT 값을 넣어 비상연락망을 등록 및 수정하는 api입니다.",
             tags = "USER", summary = "1.3.2 비상연락망 등록 및 수정 - \uD83D\uDD12 JWT")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "1000", description = "요청에 성공하였습니다."),
-            @ApiResponse(responseCode = "2000", description = "JWT를 입력해주세요."),
-            @ApiResponse(responseCode = "2001", description = "유효하지 않은 JWT입니다."),
-            @ApiResponse(responseCode = "4000", description = "데이터베이스 연결에 실패하였습니다.")
+            @ApiResponse(responseCode = "2000", description = "JWT를 입력해주세요.", content = @Content()),
+            @ApiResponse(responseCode = "2001", description = "유효하지 않은 JWT입니다.", content = @Content()),
+            @ApiResponse(responseCode = "2012", description = "전화번호 형식을 확인해주세요.", content = @Content()),
+            @ApiResponse(responseCode = "3005", description = "유저가 존재하지 않습니다.", content = @Content()),
+            @ApiResponse(responseCode = "4000", description = "데이터베이스 연결에 실패하였습니다.", content = @Content())
     })
     @ResponseBody
     @PatchMapping("/emergency")
@@ -228,19 +244,19 @@ public class UserController {
     }
 
     /**
-     * 1.3.3 Device token 저장
+     * 1.3.3 Device token 등록 및 수정
      * [PATCH] /users/deviceToken
      */
 
     @Operation(method = "PATCH",
-            description = "Header-'X-ACCESS-TOKEN'에 JWT 값을 넣어 device token 저장 api",
+            description = "Header-'X-ACCESS-TOKEN'에 JWT 값을 넣어 device token을 등록 및 수정하는 api입니다.",
             tags = "USER", summary = "1.3.3 Device Token 저장 - \uD83D\uDD12 JWT")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "1000", description = "요청에 성공하였습니다."),
-            @ApiResponse(responseCode = "2000", description = "JWT를 입력해주세요."),
-            @ApiResponse(responseCode = "2001", description = "유효하지 않은 JWT입니다."),
-            @ApiResponse(responseCode = "4000", description = "데이터베이스 연결에 실패하였습니다."),
-            @ApiResponse(responseCode = "4001", description = "데이터가 존재하지 않습니다.")
+            @ApiResponse(responseCode = "2000", description = "JWT를 입력해주세요.", content = @Content()),
+            @ApiResponse(responseCode = "2001", description = "유효하지 않은 JWT입니다.", content = @Content()),
+            @ApiResponse(responseCode = "3005", description = "유저가 존재하지 않습니다.", content = @Content()),
+            @ApiResponse(responseCode = "4000", description = "데이터베이스 연결에 실패하였습니다.", content = @Content()),
     })
     @ResponseBody
     @PatchMapping("/deviceToken")
@@ -255,6 +271,61 @@ public class UserController {
     }
 
     /**
+     * 1.3.4 비밀번호 수정
+     * [PATCH] /users/password
+     */
+    @Operation(method = "PATCH",
+            description = "Header-'X-ACCESS-TOKEN'에 JWT 값을 넣어 비밀번호를 수정하는 api입니다. originPwd가 맞는지 확인 후 업데이트합니다.",
+            tags = "USER", summary = "1.3.4 비밀번호 수정 - \uD83D\uDD12 JWT")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "1000", description = "요청에 성공하였습니다."),
+            @ApiResponse(responseCode = "2000", description = "JWT를 입력해주세요.", content = @Content()),
+            @ApiResponse(responseCode = "2001", description = "유효하지 않은 JWT입니다.", content = @Content()),
+            @ApiResponse(responseCode = "3005", description = "유저가 존재하지 않습니다.", content = @Content()),
+            @ApiResponse(responseCode = "3007", description = "비밀번호가 틀렸습니다.", content = @Content()),
+            @ApiResponse(responseCode = "4000", description = "데이터베이스 연결에 실패하였습니다.", content = @Content()),
+            @ApiResponse(responseCode = "4010", description = "비밀번호 암호화에 실패하였습니다.", content = @Content()),
+            @ApiResponse(responseCode = "4011", description = "비밀번호 복호화에 실패하였습니다.", content = @Content())
+    })
+    @ResponseBody
+    @PatchMapping("/password")
+    public BaseResponse<PatchRes> modifyPassword(@Parameter @RequestBody PatchPasswordReq patchPasswordReq) throws BaseException {
+        try {
+            int userIdByJwt= tokenUtils.getUserId();
+            PatchRes patchRes = userService.modifyPassword(userIdByJwt, patchPasswordReq);
+            return new BaseResponse<>(patchRes);
+        } catch (BaseException exception) {
+            return new BaseResponse<>((exception.getStatus()));
+        }
+    }
+
+    /**
+     * 1.3.5 로그아웃
+     * [PATCH] /users/logout
+     */
+    @Operation(method = "PATCH",
+            description = "Header-'X-ACCESS-TOKEN'에 JWT 값을 넣어 로그아웃 처리하는 api입니다.",
+            tags = "USER", summary = "1.3.5 로그아웃 API - \uD83D\uDD12 JWT")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "1000", description = "요청에 성공하였습니다."),
+            @ApiResponse(responseCode = "2000", description = "JWT를 입력해주세요.", content = @Content()),
+            @ApiResponse(responseCode = "2001", description = "유효하지 않은 JWT입니다.", content = @Content()),
+            @ApiResponse(responseCode = "3005", description = "유저가 존재하지 않습니다.", content = @Content()),
+            @ApiResponse(responseCode = "4000", description = "데이터베이스 연결에 실패하였습니다.", content = @Content()),
+    })
+    @ResponseBody
+    @PatchMapping("/logout")
+    public BaseResponse<PatchRes> logout() {
+        try {
+            int userIdByJwt = tokenUtils.getUserId();
+            userService.logout(userIdByJwt);
+            return new BaseResponse<>(new PatchRes(userIdByJwt));
+        } catch (BaseException exception) {
+            return new BaseResponse<>((exception.getStatus()));
+        }
+    }
+
+    /**
      * 1.4.1 유저 삭제
      * [DELETE] /users
      */
@@ -263,9 +334,9 @@ public class UserController {
             tags = "USER", summary = "1.4.1 유저 삭제 - \uD83D\uDD12 JWT")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "1000", description = "요청에 성공하였습니다."),
-            @ApiResponse(responseCode = "2000", description = "JWT를 입력해주세요."),
-            @ApiResponse(responseCode = "2001", description = "유효하지 않은 JWT입니다."),
-            @ApiResponse(responseCode = "4000", description = "데이터베이스 연결에 실패하였습니다.")
+            @ApiResponse(responseCode = "2000", description = "JWT를 입력해주세요.", content = @Content()),
+            @ApiResponse(responseCode = "2001", description = "유효하지 않은 JWT입니다.", content = @Content()),
+            @ApiResponse(responseCode = "4000", description = "데이터베이스 연결에 실패하였습니다.", content = @Content())
     })
     @ResponseBody
     @DeleteMapping("")
@@ -278,6 +349,4 @@ public class UserController {
             return new BaseResponse<>((exception.getStatus()));
         }
     }
-
-
 }
--- src/main/java/com/WhatIsMethIs/src/entity/User.java
+++ src/main/java/com/WhatIsMethIs/src/entity/User.java
@@ -1,9 +1,9 @@
 package com.WhatIsMethIs.src.entity;
 
 import com.WhatIsMethIs.src.dto.user.PatchEmergencyReq;
-import com.WhatIsMethIs.src.dto.user.PatchReq;
+import com.WhatIsMethIs.src.dto.user.PatchUserInfoReq;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
-import jakarta.transaction.Transactional;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,6 +12,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @Table(name = "user")
 @NoArgsConstructor
+@Schema(description = "USER 객체")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,6 +20,7 @@ public class User {
     @Column(length = 100, nullable = false)
     public String email;
     @Column(length = 200)
+    @Schema(example = "null")
     public String password;
     @Column(length = 10, nullable = false)
     public String name;
@@ -26,25 +28,33 @@ public class User {
     public String age;
     @Column(length = 20, nullable = false)
     public String phoneNumber;
+    @Column(length = 6, nullable = false)
+    public String loginCode;
     @Column(length = 20)
+    @Schema(example = "null")
     public String emergencyContact1;
     @Column(length = 20)
+    @Schema(example = "null")
     public String emergencyContact2;
     @Column
+    @Schema(example = "null")
     public String emergencyContact3;
     @Column
+    @Schema(example = "null")
     public String refreshToken;
     @Column
+    @Schema(example = "null")
     public String deviceToken;
 
     @Builder
-    public User(int id, String email, String password, String name, String age, String phoneNumber, String emergencyContact1, String emergencyContact2, String emergencyContact3, String refreshToken, String deviceToken) {
+    public User(int id, String email, String password, String name, String age, String phoneNumber, String loginCode, String emergencyContact1, String emergencyContact2, String emergencyContact3, String refreshToken, String deviceToken) {
         this.id = id;
         this.email = email;
         this.password = password;
         this.name = name;
         this.age = age;
         this.phoneNumber = phoneNumber;
+        this.loginCode = loginCode;
         this.emergencyContact1 = emergencyContact1;
         this.emergencyContact2 = emergencyContact2;
         this.emergencyContact3 = emergencyContact3;
@@ -57,17 +67,20 @@ public class User {
         this.emergencyContact2 = patchEmergencyReq.getContact2();
         this.emergencyContact3 = patchEmergencyReq.getContact3();
     }
-    public void modify(PatchReq patchReq) {
-        this.email = patchReq.getEmail();
-        this.name = patchReq.getName();
-        this.age = patchReq.getAge();
-        this.phoneNumber = patchReq.getPhoneNumber();
+    public void modify(PatchUserInfoReq patchUserInfoReq) {
+        this.email = patchUserInfoReq.getEmail();
+        this.name = patchUserInfoReq.getName();
+        this.age = patchUserInfoReq.getAge();
+        this.phoneNumber = patchUserInfoReq.getPhoneNumber();
     }
     public void saveDeviceToken(String str){
         this.deviceToken = str;
     }
     public void saveRefreshToken(String str){
         this.refreshToken = str;
+    }
+    public void modifyPassword(String str){
+        this.password = password;
     }
 
 }
