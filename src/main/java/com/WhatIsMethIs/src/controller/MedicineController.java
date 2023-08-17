add756c2f6e9e6d9c111aa9d6854f808af6ef32c
Park-MinJe <phone7101@naver.com>
2023-08-17 23:13:22 +0900

    update medicine api(2.1.3 약물 물품 번호로 검색)
    /*TODO*/
    약물 물품 번호로 검색 기능 에러

--- src/main/java/com/WhatIsMethIs/src/controller/MedicineController.java
+++ src/main/java/com/WhatIsMethIs/src/controller/MedicineController.java
@@ -1,5 +1,6 @@
 package com.WhatIsMethIs.src.controller;
 
+import com.WhatIsMethIs.config.BaseException;
 import com.WhatIsMethIs.config.BaseResponse;
 import com.WhatIsMethIs.src.dto.medicine.MedicineResponseDto;
 import com.WhatIsMethIs.src.service.MedicineService;
@@ -21,21 +22,26 @@ public class MedicineController {
     /**
      * 2.1.1 약물 정보 전체 조회
      * 2.1.2 약물명으로 검색
-     * [GET] /medicines?pageNo={pageNo}&itemName={itemName}
+     * TODO 2.1.3 약물 물품 번호로 검색
+     * [GET] /medicines?pageNo={pageNo}&itemName={itemName}&itemSeq={itemSeq}
      */
     @Operation(method = "GET", description = "약물 정보 전체를 조회하는 api로, 10개를 한 페이지로 제공", tags = "MEDICINE", summary = "2.1.1 약물 정보 전체 조회")
     @ResponseBody
     @GetMapping("")
-    public BaseResponse<MedicineResponseDto> getMedicines(@RequestParam HashMap<String, String> paramMap) throws IOException {
+    public BaseResponse<MedicineResponseDto> getMedicines(@RequestParam HashMap<String, String> paramMap) throws IOException, BaseException {
         MedicineResponseDto medicineResponseDto = null;
         
-        int pageNo = Integer.parseInt(paramMap.get("pageNo"));
+        int pageNo = 0;
+        if(paramMap.containsKey("pageNo")){
+            pageNo = Integer.parseInt(paramMap.get("pageNo"));
+        }
+
         if(paramMap.containsKey("itemName") && !paramMap.containsKey("itemSeq")){
             medicineResponseDto = medicineService.getMedicinesFromOpenApiByItemName(paramMap.get("itemName"), pageNo);
         }
-        /*else if(!paramMap.containsKey("itemName") && paramMap.containsKey("itemSeq")){
-            medicineResponseDto = medicineService.
-        }*/
+        else if(!paramMap.containsKey("itemName") && paramMap.containsKey("itemSeq")){
+            medicineResponseDto = medicineService.getMedicinesByItemSeq(paramMap.get("itemSeq"));
+        }
         else{
             medicineResponseDto = medicineService.getMedicinesFromOpenApi(pageNo);
         }
